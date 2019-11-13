9287d27ec76497c995ea447031e61f004056ccf3
Ivan Malshchukov <nummyn0rih@gmail.com>
2019-11-13 15:08:46 +0300

    add CodeClimate, Eslint configuration & fix linter errors

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { greeting } from '..';
+import greeting from '..';
 
-console.log(`Welcome to the Brain Games! \n`);
+console.log('Welcome to the Brain Games! \n');
 
 greeting();
