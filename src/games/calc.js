b6f57b6378b864f63e949bd3b69c890290a5ca02
Ivan Malshchukov <nummyn0rih@gmail.com>
2020-01-01 18:18:20 +0300

    fix magic number

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,16 +3,18 @@ import gameEngine from '..';
 import getRandom from '../get-random';
 
 const func = () => {
-  const sign = getRandom(3);
+  const signs = ['+', '-', '*'];
+
+  const sign = signs[getRandom(3)];
   const num1 = getRandom(25);
   const num2 = getRandom(25);
 
   switch (sign) {
-    case 0:
+    case '+':
       return cons(`${num1} + ${num2}`, num1 + num2);
-    case 1:
+    case '-':
       return cons(`${num1} - ${num2}`, num1 - num2);
-    case 2:
+    case '*':
       return cons(`${num1} * ${num2}`, num1 * num2);
     default:
       return null;
