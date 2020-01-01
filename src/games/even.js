86a3af9ff145c3c946966e48ae81e11d782704cd
Ivan Malshchukov <nummyn0rih@gmail.com>
2020-01-01 17:22:27 +0300

    fix naming

--- src/games/even.js
+++ src/games/even.js
@@ -5,9 +5,9 @@ import getRandom from '../get-random';
 const isEven = (num) => num % 2 === 0;
 
 const func = () => {
-  const number = getRandom(99);
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
-  return cons(number, correctAnswer);
+  const question = getRandom(99);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return cons(question, correctAnswer);
 };
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
