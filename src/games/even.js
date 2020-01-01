51c3bd9998ce6a7d1556a73ec9870d7bd7972944
Ivan Malshchukov <nummyn0rih@gmail.com>
2020-01-01 17:18:19 +0300

    fix predict func

--- src/games/even.js
+++ src/games/even.js
@@ -2,11 +2,12 @@ import { cons } from '@hexlet/pairs';
 import gameEngine from '..';
 import getRandom from '../get-random';
 
-const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const isEven = (num) => num % 2 === 0;
 
 const func = () => {
   const number = getRandom(99);
-  return cons(number, isEven(number));
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  return cons(number, correctAnswer);
 };
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
