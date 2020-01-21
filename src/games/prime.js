83c6b6c1d8703c81dbf881591f34a943808effcb
nummyn0rih <nummyn0rih@gmail.com>
2020-01-22 01:35:11 +0300

    refactoring predicate

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,19 +5,19 @@ import getRandom from '../get-random';
 const isEven = (num) => (num % 2 === 0);
 
 const isPrime = (num) => {
-  if (num === 2) return 'yes';
-  if (num === 1 || isEven(num)) return 'no';
+  if (num === 2) return true;
+  if (num <= 1 || isEven(num)) return false;
   let divisor = 2;
-  while (divisor ** 2 < num) {
-    if (num % divisor === 0) return 'no';
+  while (divisor ** 2 <= num) {
+    if (num % divisor === 0) return false;
     divisor += 1;
   }
-  return 'yes';
+  return true;
 };
 
 const createData = () => {
   const number = getRandom(1, 322);
-  return cons(number, isPrime(number));
+  return cons(number, isPrime(number) ? 'yes' : 'no');
 };
 
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
