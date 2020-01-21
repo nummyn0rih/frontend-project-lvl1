87812ddfc2763e685f2639b8010b5eeb7c439cfb
nummyn0rih <nummyn0rih@gmail.com>
2020-01-22 00:52:00 +0300

    fix naming

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,12 +2,12 @@ import { cons } from '@hexlet/pairs';
 import gameEngine from '..';
 import getRandom from '../get-random';
 
-const getNod = (num1, num2) => (num1 % num2 === 0 ? num2 : getNod(num2, num1 % num2));
+const getGCD = (num1, num2) => (num1 % num2 === 0 ? num2 : getGCD(num2, num1 % num2));
 
 const createData = () => {
   const num1 = getRandom(0, 99);
   const num2 = getRandom(0, 99);
-  const result = num1 > num2 ? getNod(num1, num2) : getNod(num2, num1);
+  const result = num1 > num2 ? getGCD(num1, num2) : getGCD(num2, num1);
   return cons(`${num1} ${num2}`, result);
 };
 
