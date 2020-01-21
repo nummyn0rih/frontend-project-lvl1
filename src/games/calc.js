db9731e22bd59ff048efa39fadc8feaf480aec59
nummyn0rih <nummyn0rih@gmail.com>
2020-01-22 00:48:33 +0300

    refactoring

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,21 +4,29 @@ import getRandom from '../get-random';
 
 const signs = ['+', '-', '*'];
 
-const createData = () => {
-  const sign = signs[getRandom(0, signs.length)];
-  const num1 = getRandom(0, 25);
-  const num2 = getRandom(0, 25);
+const createQuestion = ({ sign, num1, num2 }) => `${num1} ${sign} ${num2}`;
 
+const createAnswer = ({ sign, num1, num2 }) => {
   switch (sign) {
     case '+':
-      return cons(`${num1} + ${num2}`, num1 + num2);
+      return num1 + num2;
     case '-':
-      return cons(`${num1} - ${num2}`, num1 - num2);
+      return num1 - num2;
     case '*':
-      return cons(`${num1} * ${num2}`, num1 * num2);
+      return num1 * num2;
     default:
       return null;
   }
+};
+
+const createData = () => {
+  const exerciseData = {
+    sign: signs[getRandom(0, signs.length)],
+    num1: getRandom(0, 25),
+    num2: getRandom(0, 25),
+  };
+
+  return cons(createQuestion(exerciseData), createAnswer(exerciseData));
 };
 
 const rule = 'What is the result of the expression?';
